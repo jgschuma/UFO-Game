1144bef8a968e7466c577e4f100be9355da6ae01
jgschuma <47636951+jgschuma@users.noreply.github.com>
2022-09-13 11:35:22 -0400

    Working on item pickups

---
+++ Assets/Scripts/Shield.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shield : MonoBehaviour
+{
+    public Rigidbody2D ShieldRigidBody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ShieldRigidBody.freezeRotation = true;
+    }
+
+    void OnTriggerEnter2D (Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Pickup();
+        }
+    }
+
+    void Pickup()
+    {
+        Debug.Log("Power up picked up");
+
+    }
+}
