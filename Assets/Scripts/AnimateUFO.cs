0f9db3d80ac918473405a82722fa7b472568d6ed
ECHICKS <47527393+ECHICKS@users.noreply.github.com>
2022-09-20 15:05:09 -0400

    UFO Animations are working
    Let's
    
    fucking
    
    GOOOOOOOOOOO

---
+++ Assets/Scripts/AnimateUFO.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+
+public class AnimateUFO : MonoBehaviour
+{
+    private Animator anim;
+    private PlayerController cont;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cont = GetComponent<PlayerController>();
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Set the movement vector for use in FixedUpdate()
+        anim.SetInteger("HoriInput", Math.Sign(cont.horizontalInput));
+        anim.SetInteger("VertiInput", Math.Sign(cont.verticalInput));
+    }
+}
