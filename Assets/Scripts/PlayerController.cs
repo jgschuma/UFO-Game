d7cfdc7033bba17827eeaaeb2e1fc41825cfa602
jgschuma <47636951+jgschuma@users.noreply.github.com>
2022-09-20 01:19:24 -0400

    fixed some gameObjects not being assigned in the inspector

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -15,14 +15,6 @@ public class PlayerController : MonoBehaviour
     Vector3 movement;
     public Animator anim;
 
-    // /*These variables allow for the use of the tractorBeam
-    // game object*/
-    // public GameObject tractorBeam;
-    // public bool hasItem;
-    // public bool beamOnCooldown;
-    // public float beamCoolDuration;
-
-
 
     // Start is called when the game starts
     private void Start()
@@ -31,7 +23,6 @@ public class PlayerController : MonoBehaviour
         UFO_Rigidbody.freezeRotation = true;
 
         anim = GetComponent<Animator>();
-
     }
 
     // Update is called once per frame
@@ -49,40 +40,11 @@ public class PlayerController : MonoBehaviour
 
         movement = new Vector3(horizontalInput, verticalInput, 0).normalized;
 
-        // calls manager for the tractor beam
-        //tractorBeamController();
     }
 
     // We use FixedUpdate to actually move the player to avoid frame rate causing a difference in move speed
     void FixedUpdate(){
         UFO_Rigidbody.MovePosition(transform.position + movement * moveSpeed* Time.deltaTime);
     }
-
-
-    // // This method controls the TractorBeam game object
-    // public void tractorBeamController()
-    // {
-    //     // If key is pressed and no item is held, activate the tractorbeam
-    //     if (Input.GetButton("Fire1") && hasItem == false && beamOnCooldown == false)
-    //     {
-    //         tractorBeam.SetActive(true);
-    //     } // If key is pressed and an Item is held, drop the item
-    //     else if (Input.GetButton("Fire1") && hasItem == true && beamOnCooldown == false){
-    //         Debug.Log("Item has been dropped");
-    //         hasItem = false;
-    //         tractorBeam.SetActive(false);
-    //         StartCoroutine(BeamCooldown());
-    //     } // Else keep the tractor beam off
-    //     else
-    //         tractorBeam.SetActive(false);
-    // }
-
-    // public IEnumerator BeamCooldown(){
-    //     beamOnCooldown = true;
-
-    //     yield return new WaitForSeconds(beamCoolDuration);
-
-    //     beamOnCooldown = false;
-    // }
 
 }
