d87a052f282302f66b7800d68a42f2b38e7c179d
Austin Matias <austinjmatias@gmail.com>
2022-09-27 11:52:48 -0400

    Redid some movement, added bullets

--- Assets/Scripts/Enemy_AirShoot.cs
+++ Assets/Scripts/Enemy_AirShoot.cs
@@ -4,39 +4,63 @@ using UnityEngine;
 
 public class Enemy_AirShoot : MonoBehaviour
 {
+    [Tooltip("The player to follow.")]
     public GameObject player;
+    [Tooltip("Speed of the player.")]
     public float speed;
+    [Tooltip("Distance the enemy can wander away from their home base.")]
     public float wanderRadius;
+    [Tooltip("Distance from which the enemy will be alerted to the player")]
     public float alertRadius;
+    [Tooltip("Minimum distance from the player the enemy will keep")]
     public float shootingDistance;
+    [Tooltip("Time in between shots")]
+    public float rateOfFire;
     public Rigidbody2D enemyRigidBody;
+    public Transform firePoint;
+    public GameObject bulletPrefab;
+
 
     private Vector2 homebase;
     private float distanceToPlayer;
     private float distanceFromHome;
     private Vector2 direction;
+    private bool allowFire;
+    private bool facingRight;
     // Start is called before the first frame update
     void Start()
     {
         enemyRigidBody.freezeRotation = true;
         homebase = transform.position;
+        allowFire = true;
+        facingRight = true;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
+    private void FixedUpdate() {
         //update distances
         distanceToPlayer = Vector2.Distance(transform.position, player.transform.position);
         distanceFromHome = Vector2.Distance(transform.position, homebase);
 
         // If the player is within alert radius, and inside shooting range
         if (distanceToPlayer <= shootingDistance && distanceToPlayer <= alertRadius){
+            //if we face right and player is left, or we face left and player is right
+            if(facingRight == true && player.transform.position.x < transform.position.x || facingRight == false && player.transform.position.x > transform.position.x){
+                flip();
+            }
+            //aim at player by setting the firepoint to point at the player
+
             //shoot at the player
-            shoot();
+            if (allowFire) {
+                StartCoroutine(shoot());
+            }
         }
 
         // If the player is within the alert radius, outside shooting distance, and within wander radius
         if (distanceToPlayer <= alertRadius && distanceToPlayer > shootingDistance && distanceFromHome < wanderRadius){
+            //if we face right and player is left, or we face left and player is right
+            if(facingRight == true && player.transform.position.x < transform.position.x || facingRight == false && player.transform.position.x > transform.position.x){
+                flip();
+            }
             //go towards player
             transform.position = Vector2.MoveTowards(this.transform.position, player.transform.position, speed * Time.deltaTime);
         }
@@ -46,10 +70,20 @@ public class Enemy_AirShoot : MonoBehaviour
             //go home
             transform.position = Vector2.MoveTowards(this.transform.position, homebase, speed * Time.deltaTime);
         }
-         
     }
 
-    void shoot(){
+    void flip(){
+        facingRight = !facingRight;
+        transform.Rotate(0, 180, 0);
+    }
 
+    IEnumerator shoot() {
+        allowFire = false;
+        yield return new WaitForSeconds(rateOfFire);
+        GameObject newBullet = Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+
+        //newBullet.GetComponent<Rigidbody2D>().velocity = new Vector2(bulletSpeed * Time.fixedDeltaTime, 0f);
+
+        allowFire = true;
     }
 }
