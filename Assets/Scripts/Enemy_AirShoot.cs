94f65601272bf5172f60e4258059d10dee986f05
Austin Matias <austinjmatias@gmail.com>
2022-09-27 16:30:13 -0400

    Finished basic implementation of Air Shooting AI

--- Assets/Scripts/Enemy_AirShoot.cs
+++ Assets/Scripts/Enemy_AirShoot.cs
@@ -16,6 +16,8 @@ public class Enemy_AirShoot : MonoBehaviour
     public float shootingDistance;
     [Tooltip("Time in between shots")]
     public float rateOfFire;
+    public bool isTrackingPlayer;
+    public bool isShootingPlayer;
     public Rigidbody2D enemyRigidBody;
     public Transform firePoint;
     public GameObject bulletPrefab;
@@ -24,7 +26,7 @@ public class Enemy_AirShoot : MonoBehaviour
     private Vector2 homebase;
     private float distanceToPlayer;
     private float distanceFromHome;
-    private Vector2 direction;
+    private Vector3 direction;
     private bool allowFire;
     private bool facingRight;
     // Start is called before the first frame update
@@ -41,6 +43,7 @@ public class Enemy_AirShoot : MonoBehaviour
         distanceToPlayer = Vector2.Distance(transform.position, player.transform.position);
         distanceFromHome = Vector2.Distance(transform.position, homebase);
 
+
         // If the player is within alert radius, and inside shooting range
         if (distanceToPlayer <= shootingDistance && distanceToPlayer <= alertRadius){
             //if we face right and player is left, or we face left and player is right
@@ -50,6 +53,7 @@ public class Enemy_AirShoot : MonoBehaviour
             //aim at player by setting the firepoint to point at the player
 
             //shoot at the player
+            isShootingPlayer = true;
             if (allowFire) {
                 StartCoroutine(shoot());
             }
@@ -62,12 +66,15 @@ public class Enemy_AirShoot : MonoBehaviour
                 flip();
             }
             //go towards player
+            isTrackingPlayer = true;
             transform.position = Vector2.MoveTowards(this.transform.position, player.transform.position, speed * Time.deltaTime);
         }
 
         // If outside of wander radius or player is outside of alert radius
         if (distanceFromHome >= wanderRadius || distanceToPlayer > alertRadius){
             //go home
+            isTrackingPlayer = false;
+            isShootingPlayer = false;
             transform.position = Vector2.MoveTowards(this.transform.position, homebase, speed * Time.deltaTime);
         }
     }
@@ -79,11 +86,12 @@ public class Enemy_AirShoot : MonoBehaviour
 
     IEnumerator shoot() {
         allowFire = false;
+        direction = player.transform.position - firePoint.transform.position;
+        float rotationZ = Mathf.Atan2 (direction.y, direction.x) * Mathf.Rad2Deg;
+        firePoint.transform.rotation = Quaternion.Slerp(firePoint.transform.rotation, Quaternion.Euler(0, 0, rotationZ), 100 * Time.deltaTime);
         yield return new WaitForSeconds(rateOfFire);
-        GameObject newBullet = Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
-
-        //newBullet.GetComponent<Rigidbody2D>().velocity = new Vector2(bulletSpeed * Time.fixedDeltaTime, 0f);
-
+        //shoot
+        Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
         allowFire = true;
     }
 }
