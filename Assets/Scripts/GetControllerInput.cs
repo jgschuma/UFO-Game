3a9fa44a6d39b408d2604eb562e3a471952daad9
ECHICKS <47527393+ECHICKS@users.noreply.github.com>
2022-09-27 19:25:34 -0400

    Guided Missile Base
    +Controllable Guided Missile object
    +New animations and animator for the Guided Missile (flying in 8 cardinal directions)
    +New script GetControllerInput for getting player input
    +New script GuidedMissileController
    =Modified PlayerController to get input from attached GetControllerInput script
    =Added dedicated method in ProjectileDirection for converting degrees into radians
    =Simplified UFO animations and sheet

---
+++ Assets/Scripts/GetControllerInput.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GetControllerInput : MonoBehaviour
+{
+    public float horizontalInput;
+    public float verticalInput;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Checks to see if the horzontal or vertical inputs are being pressed
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
+    }
+}
--- Assets/Scripts/ProjectileDirection.cs
+++ Assets/Scripts/ProjectileDirection.cs
@@ -14,17 +14,21 @@ public class ProjectileDirection : MonoBehaviour
     void Start()
     {
         proj_Rigidbody = GetComponent<Rigidbody2D>();
-        direction = direction * Math.PI / 180;
     }
 
     // Update is called once per frame
     void Update()
     {
-        movement = new Vector3((float)(Math.Sin(direction) * speed), (float)(Math.Cos(direction) * speed), 0).normalized;
+        movement = new Vector3((float)(Math.Sin(ConvertToRadians(direction)) * speed), (float)(Math.Cos(ConvertToRadians(direction)) * speed), 0).normalized;
     }
 
     void FixedUpdate()
     {
         proj_Rigidbody.MovePosition(transform.position + movement * speed * Time.deltaTime);
+    }
+
+    double ConvertToRadians(double _degrees)
+    {
+        return _degrees * Math.PI / 180;
     }
 }
