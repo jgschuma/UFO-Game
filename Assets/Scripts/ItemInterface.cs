efec84696db0904f2cf01ad477410f1a6cbe03a9
Jacob Schumacher <jgschuma@uncg.edu>
2022-09-29 14:32:04 -0400

    Working on Gunner Implementation

---
+++ Assets/Scripts/ItemInterface.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IPickup
+{
+    // Methods required for ItemPickups
+    void Start();
+
+    void OnTriggerEnter2D(Collider2D other);
+
+    void Pickup();
+}
