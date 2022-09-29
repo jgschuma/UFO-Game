ec030986289218d5bbe36d119faa79c66345e466
Austin Matias <austinjmatias@gmail.com>
2022-09-29 14:23:02 -0400

    Adding bullet

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public Rigidbody2D rigidBody;
+    public float despawnTime, damage, speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidBody.velocity = transform.right * speed;
+        StartCoroutine(CountdownTimer());
+    }
+
+    void OnCollisionEnter2D(Collision2D col) {
+        Destroy(gameObject);
+    }
+
+    IEnumerator CountdownTimer(){
+        yield return new WaitForSeconds(despawnTime);
+        Destroy(gameObject);
+    }
+}
