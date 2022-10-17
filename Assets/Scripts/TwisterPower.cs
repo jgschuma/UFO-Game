7006def85a807d28ab996f235bdc5610a7983763
ECHICKS <echicks@uncg.edu>
2022-10-17 12:54:53 -0400

    Twister movement partial complete
    +Twister maintains speed during start up, speeds up greatly when active, then slows to a stop when it's over. Setting the direction of the UFO is bugged and it will frequently go in the wrong direction

--- Assets/Scripts/TwisterPower.cs
+++ Assets/Scripts/TwisterPower.cs
@@ -6,7 +6,9 @@ using System;
 public class TwisterPower : MonoBehaviour
 {
     public GameObject UFO;
-    
+    public float twisterSpeed = 400f;
+    public float winddownDecel = 8f;
+
     public GetControllerInput contInput;
     public Animator anim;
     
@@ -27,18 +29,30 @@ public class TwisterPower : MonoBehaviour
             UFO.GetComponent<BeamController>().enabled = false;
             contInput.enabled = false;
             anim.SetBool("twisterOn", true);
+            //Maintain movement speed and direction and apply it to ProjectileDirection
+            float xVelocity = UFO.GetComponent<PlayerController>().movement.x;
+            float yVelocity = UFO.GetComponent<PlayerController>().movement.y;
+            UFO.GetComponent<ProjectileDirection>().speed = (float)Math.Sqrt(Math.Pow(xVelocity, 2) + Math.Pow(yVelocity, 2));
+            UFO.GetComponent<ProjectileDirection>().direction = Math.Atan(xVelocity / yVelocity) * (180 / Math.PI);
+            UFO.GetComponent<ProjectileDirection>().enabled = true;
+        }
+        //Twister has picked up to full speed
+        else if (anim.GetCurrentAnimatorStateInfo(0).IsName("TwisterActive"))
+        {
+            UFO.GetComponent<ProjectileDirection>().speed = twisterSpeed;
+        }
+        //Twister is winding down
+        else if (anim.GetCurrentAnimatorStateInfo(0).IsName("TwisterWinddown") && UFO.GetComponent<ProjectileDirection>().speed != 0)
+        {
+            UFO.GetComponent<ProjectileDirection>().speed = Math.Max(UFO.GetComponent<ProjectileDirection>().speed - winddownDecel, 0);
         }
         //The twister is over, restore control
         else if (anim.GetCurrentAnimatorStateInfo(0).IsName("TwisterOver"))
         {
             anim.SetBool("twisterOn", false);
+            UFO.GetComponent<ProjectileDirection>().enabled = false;
             UFO.GetComponent<BeamController>().enabled = true;
             contInput.enabled = true;
-        }
-        //Twister is in motion
-        else
-        {
-            
         }
     }
 }
