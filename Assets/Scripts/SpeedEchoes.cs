d3e719a50d93a88c41baedb39826f01658b8e74d
ECHICKS <47527393+ECHICKS@users.noreply.github.com>
2022-10-18 16:30:06 -0400

    Speed echoes match the sprite of the UFO now
    +and also miscellaneous tinkering with health animations

--- Assets/Scripts/SpeedEchoes.cs
+++ Assets/Scripts/SpeedEchoes.cs
@@ -7,18 +7,21 @@ public class SpeedEchoes : MonoBehaviour
     public int numOfEchoes = 3;
     public float timeBetweenEchoes = 0.2f;
     public GameObject speedEcho;
+    public Object[] echoSprites;
 
-    //public Sprite[] sprites;
     float timeLeftUntilEcho = 0;
     PlayerController playerCon;
 
     // Start is called before the first frame update
     void Start()
     {
-        //sprites = Resources.LoadAll<Sprite>("Assets/Sprites/Playable_UFO_Echoes-Sheet.png");
-        //speedEcho = (GameObject)Resources.Load("Assets/Prefabs/SpeedEcho.prefab", typeof(GameObject));
         speedEcho.GetComponent<ProjectileDirection>().despawnTime = numOfEchoes * timeBetweenEchoes;
         playerCon = GetComponent<PlayerController>();
+        echoSprites = UnityEditor.AssetDatabase.LoadAllAssetsAtPath("Assets/Sprites/Playable_UFO_Echoes-Sheet.png");
+        //The full sheet and Sprite 0 are swapped for some reason, swap them back
+        Object sprite0 = echoSprites[0];
+        echoSprites[0] = echoSprites[1];
+        echoSprites[1] = sprite0;
     }
 
     // Update is called once per frame
@@ -31,14 +34,12 @@ public class SpeedEchoes : MonoBehaviour
             if (timeLeftUntilEcho <= 0)
             {
                 timeLeftUntilEcho += timeBetweenEchoes;
-                //Assets/Sprites/Playable_UFO-Sheet.png
                 //Isolate the exact sprite of the UFO
                 string spriteID = GetComponent<SpriteRenderer>().sprite.name;
                 spriteID = spriteID.Substring(spriteID.IndexOf('O') + 8);
-                Debug.Log("Sprite ID: " + spriteID);
+                //Debug.Log("Sprite ID: " + spriteID);
                 //Change sprite of echo to match current sprite
-                //speedEcho.GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("Assets/Sprites/Playable_UFO_Echoes-Sheet-" + spriteID + ".png");
-                //speedEcho.GetComponent<SpriteRenderer>().sprite = sprites[int.Parse(spriteID)];
+                speedEcho.GetComponent<SpriteRenderer>().sprite = (Sprite)echoSprites[int.Parse(spriteID) + 1];
                 Instantiate(speedEcho, transform.position, Quaternion.Euler(0, 0, 0));
             }
         }
