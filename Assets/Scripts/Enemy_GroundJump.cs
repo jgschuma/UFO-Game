86b281d7c8be85e7d325124fec8e93de5a15a12a
Austin Matias <austinjmatias@gmail.com>
2022-10-20 17:13:13 -0400

    Implementation done on GroundJump

--- Assets/Scripts/Enemy_GroundJump.cs
+++ Assets/Scripts/Enemy_GroundJump.cs
@@ -8,7 +8,7 @@ public class Enemy_GroundJump : MonoBehaviour
     public float moveSpeed;
     public Transform groundCheckPoint;
     public Transform wallCheckPoint;
-    public float circleRadius;
+    public float checksCircleRadius;
 
     public LayerMask groundLayer;
     private bool touchingGround;
@@ -16,10 +16,18 @@ public class Enemy_GroundJump : MonoBehaviour
 
     [Header("Jump Attack")]
     public float jumpHeight;
+    public float jumpCooldown;
+    private bool allowJump;
     public Transform player;
     public Transform feetPlantCheck;
     public Vector2 boxSize;
     private bool isGrounded;
+
+    [Header("For Seeing Player")]
+    public Transform alertCenter;
+    public Vector2 alertZone;
+    public LayerMask playerLayer;
+    private bool canSeePlayer;
 
     [Header("Other")]
     public Rigidbody2D rb;
@@ -29,18 +37,27 @@ public class Enemy_GroundJump : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-
+        allowJump = true;
+        Physics2D.IgnoreLayerCollision(0,15, true);
+        //Physics2D.IgnoreLayerCollision(0,0, true);
+        player = GameObject.Find("UFO").transform;
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        touchingGround = Physics2D.OverlapCircle(groundCheckPoint.position, circleRadius, groundLayer);
-        touchingWall = Physics2D.OverlapCircle(wallCheckPoint.position, circleRadius, groundLayer);
+        touchingGround = Physics2D.OverlapCircle(groundCheckPoint.position, checksCircleRadius, groundLayer);
+        touchingWall = Physics2D.OverlapCircle(wallCheckPoint.position, checksCircleRadius, groundLayer);
         isGrounded = Physics2D.OverlapBox(feetPlantCheck.position, boxSize, 0, groundLayer);
-        //Patrolling();
-        if(Input.GetKeyDown(KeyCode.Space)){
-            JumpAttack();
+        canSeePlayer = Physics2D.OverlapBox(alertCenter.position, alertZone, 0, playerLayer);
+
+        if (!canSeePlayer && isGrounded){
+            Patrolling();
+        }
+        if (canSeePlayer && isGrounded){
+            if (allowJump) {
+                StartCoroutine(JumpAttack());
+            }
         }
     }
 
@@ -51,11 +68,19 @@ public class Enemy_GroundJump : MonoBehaviour
         rb.velocity = new Vector2(moveSpeed*moveDirection, rb.velocity.y);
     }
 
-    void JumpAttack(){
+    IEnumerator JumpAttack(){
+        allowJump = false;
         float distanceFromPlayer = player.position.x - transform.position.x;
+        //set the enemy to face the correct position
+        if ((distanceFromPlayer < 0 && facingRight) || (distanceFromPlayer > 0 && !facingRight)){
+            flip();
+        }
+        yield return new WaitForSeconds(jumpCooldown);
+        //jump!
         if (isGrounded){
             rb.AddForce(new Vector2(distanceFromPlayer, jumpHeight), ForceMode2D.Impulse);
         }
+        allowJump = true;
     }
 
     void flip(){
@@ -65,8 +90,17 @@ public class Enemy_GroundJump : MonoBehaviour
     }
 
     private void OnDrawGizmos() {
+        //BLUE -- For checking if about to patroll into a wall or off a cliff
         Gizmos.color = Color.blue;
-        Gizmos.DrawWireSphere(groundCheckPoint.position, circleRadius);
-        Gizmos.DrawWireSphere(wallCheckPoint.position, circleRadius);
+        Gizmos.DrawWireSphere(groundCheckPoint.position, checksCircleRadius);
+        Gizmos.DrawWireSphere(wallCheckPoint.position, checksCircleRadius);
+
+        //GREEN -- For checking if grounded in order to jump
+        Gizmos.color = Color.green;
+        Gizmos.DrawCube(feetPlantCheck.position, boxSize);
+
+        //RED -- Alert Area
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireCube(alertCenter.position, alertZone);
     }
 }
