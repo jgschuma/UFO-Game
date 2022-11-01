25300321882be4145b09c8ca6c125cca6abe7d56
ECHICKS <47527393+ECHICKS@users.noreply.github.com>
2022-11-01 14:47:50 -0400

    Various changes
    +New prefab FireGeyser with associated sprites, animator, animations, and scripts
    +Added functionality to OneWayWall
    +New scripts breaksOneWay and OneWayHandler
    +Added environmental damage, with changes to EntityHealth script to accomodate

---
+++ Assets/Scripts/GeyserSpewFlame.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class GeyserSpewFlame : MonoBehaviour
+{
+    public GameObject flamePrefab;
+    public float flameDelay = 2f;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(SpewFlame());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator SpewFlame()
+    {
+        yield return new WaitForSeconds(flameDelay);
+        //Debug.Log("Dir: " + -transform.rotation.eulerAngles.z);
+        flamePrefab.GetComponent<ProjectileDirection>().direction = -transform.rotation.eulerAngles.z;
+        Instantiate(flamePrefab, transform.position, Quaternion.Euler(0, 0, 0));
+        StartCoroutine(SpewFlame());
+    }
+}
