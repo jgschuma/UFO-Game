76e169eb62badfeaebb0c980e051f8e0f0a37f97
Austin Matias <austinjmatias@gmail.com>
2022-11-01 14:10:16 -0400

    Initial sound implementation test. Also adding sprite 0 for ground jump

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -5,6 +5,13 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+
+    public AudioSource menuMusicSource;
+
+    void Start(){
+        menuMusicSource.Play();
+    }
+
     public void PlayGame(){
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
         AustinEventManager.StartGame();
