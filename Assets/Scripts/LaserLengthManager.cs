dbc65f28870af66b3339f6e6e04e4cb10db0e95b
jgschuma <47636951+jgschuma@users.noreply.github.com>
2022-11-08 00:25:46 -0500

    First implementation of laser power

---
+++ Assets/Scripts/LaserLengthManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaserLengthManager : MonoBehaviour
+{
+    public GameObject FirePoint;
+    public GameObject LaserLine;
+    public GameObject LaserImpact;
+    public float ImpactOffset;
+    private Vector3 ImpactPosition;
+
+    float Distance;
+    Ray ray;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit2D raycastHit2D = Physics2D.Raycast(FirePoint.transform.position, Vector3.up);
+        if(raycastHit2D){
+            Vector3 HitLocation = raycastHit2D.point;
+            LaserImpact.transform.position = HitLocation + new Vector3(0, ImpactOffset, 0);
+
+            
+            Distance = Vector3.Distance(FirePoint.transform.position, HitLocation);
+            LaserLine.transform.localScale = new Vector3(1, (Distance/16f), 1);
+
+            
+            Vector3 middlePoint = (FirePoint.transform.position + HitLocation)/2f;
+            LaserLine.transform.position = middlePoint;
+
+            // LaserImpact.transform.position = HitLocation;
+        }
+    }   
+}
---
+++ Assets/Scripts/LaserPower.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaserPower : MonoBehaviour
+{
+    public GameObject Laser;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetButton("Fire2")){
+            Laser.SetActive(true);
+        }else{
+            Laser.SetActive(false);
+        }
+    }
+}
