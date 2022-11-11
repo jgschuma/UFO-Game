a219ebdc857ef5ad367e5e47ed9de23973a95207
jgschuma <47636951+jgschuma@users.noreply.github.com>
2022-11-11 17:16:17 -0500

    Warp now allows the player to warp while holding the warpPower

--- Assets/Scripts/WarpPower.cs
+++ Assets/Scripts/WarpPower.cs
@@ -4,5 +4,29 @@ using UnityEngine;
 
 public class WarpPower : MonoBehaviour
 {
+    public GameObject WarpPrefab;
+    public Camera MainCam;
+    public Transform WarpLocation;
+    private bool WarpInProgress = false;
+    private bool isFinished;
+    public GameObject Player;
+    private Animator WarpAnim;
     // This does nothing as of yet
+    
+    void Update(){
+        if(Input.GetButton("Fire2") && WarpInProgress == false){
+            WarpInProgress = true;
+            GameObject WarpEffect = Instantiate(WarpPrefab, transform.position, Quaternion.identity);
+            WarpEffect.GetComponent<Canvas>().worldCamera = MainCam;
+            WarpAnim = WarpEffect.GetComponent<Animator>();
+        }
+
+        if(WarpAnim != null && WarpAnim.GetBool("StartDone") && WarpInProgress == true){
+            Player.transform.position = WarpLocation.position;
+
+            WarpInProgress = false;
+            WarpAnim.SetBool("TeleportDone", true);
+        }
+    }
+
 }
