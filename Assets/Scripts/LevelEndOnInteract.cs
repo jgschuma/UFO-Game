232b4524d8ad256a4977fa688f4b183aa86d4e60
ECHICKS <echicks@uncg.edu>
2022-11-13 16:22:04 -0500

    Mothership chair added
    +New prefab MothershipChair added with associated sprites, animator, animation, and script

---
+++ Assets/Scripts/LevelEndOnInteract.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelEndOnInteract : MonoBehaviour
+{
+    public GameObject player;
+    Animator anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+/*    // Update is called once per frame
+    void Update()
+    {
+
+    }*/
+
+    void OnTriggerStay2D(Collider2D other)
+    {
+        //It's the player, end the level
+        if(other.gameObject.name == player.name && other.gameObject.tag == player.tag)
+        {
+            other.gameObject.transform.Find("Main Camera").parent = null;
+            other.gameObject.transform.Find("UI").parent = null;
+            Destroy(other.gameObject);
+            anim.SetTrigger("levelEnded");
+        }
+    }
+}
